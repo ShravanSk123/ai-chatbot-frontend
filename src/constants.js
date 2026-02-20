c570b51a81084ce606eb69a3765e43300a746034
ShravanSk123 <46959596+ShravanSk123@users.noreply.github.com>
2026-02-21 01:03:12 +0530

    Update API_BASE_URL to new backend endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 //export const API_BASE_URL = 'http://localhost:5207/api/chat';
-export const API_BASE_URL = 'https://ai-chatbot-y31e.onrender.com/api/Chat';
+export const API_BASE_URL = 'https://ssk--ai-chatbot-backend--vqs9rzqpkddg.code.run/api/Chat';
